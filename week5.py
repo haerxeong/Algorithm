6f5e18a741243ec2c07767ae783c9a16cb0510b3
haerxeong <haeryeong04@naver.com>
2024-08-20 14:25:11 +0900

    1021

--- week5.py
+++ week5.py
@@ -1,7 +1,7 @@
 #stack, queue, dequeue, priority queue
 
 #1874번: 스택 수열
-'''n = int(input())
+n = int(input())
 li = [int(input()) for _ in range(n)]
 stack, ans = [], []
 current = 1
@@ -16,17 +16,49 @@ while current <= n + 1:
         li.pop(0)
     print(stack)
 print('NO' if stack else '\n'.join(ans))
-print(stack)'''
+print(stack)
 
 #1417번: 국회의원 선거
 
-
 #큐
-#
+#1158번: 요세푸스 문제
+from collections import deque
+N, K = map(int, input().split())
+cnt = 0
+ans = []
+
+Q = deque([i for i in range(1, N + 1)])
+while Q:
+    if cnt == K - 1:
+        ans.append(Q.popleft())
+    else: Q.append(Q.popleft())
+    cnt = (cnt + 1) % K
+
+print(f"<{', '.join(map(str, ans))}>")
+
+#1021번: 회전하는 큐
+from collections import deque
+
+N, M = map(int, input().split())
+target = deque(map(int, input().split()))
+Deq = deque(i for i in range(1, N + 1))
+cnt = 0
+
+while target:
+    if target[0] == Deq[0]:
+        Deq.popleft()
+        target.popleft()
+    else:
+        if Deq.index(target[0]) <= len(Deq) // 2:
+            Deq.append(Deq.popleft())
+        else:
+            Deq.appendleft(Deq.pop())
+        cnt += 1
+print(cnt)
 
 #덱
 #2164번: 카드2
-'''from collections import deque
+from collections import deque
 
 N = int(input())
 dq = deque(i for i in range(1, N + 1))
@@ -35,23 +67,41 @@ while len(dq) != 1:
     dq.popleft()
     dq.append(dq.popleft())
 
-print(dq[0])'''
+print(dq[0])
 
-#8892번: 팰린드롬
+#우선순위 큐
+#1927번: 최소 힙
+input = __import__('sys').stdin.readline
+from heapq import heappush, heappop
+hq = []
+
+for i in range(int(input())):
+    x = int(input())
+    if x:
+        heappush(hq, x)
+    else:
+        print(0 if not hq else heappop(hq))
+
+#11279번: 최대 힙
+input = __import__('sys').stdin.readline
+from heapq import heappush, heappop
+
+hq = []
 for _ in range(int(input())):
-    k = int(input())
-    flag = False
-    li = list(input() for _ in range(k))
-    
-    for i in range(k - 1):
-        for j in range(i + 1, k):
-            if li[i] + li[j] == (li[i] + li[j])[::-1]:
-                print(li[i] + li[j])
-                flag = True
-                break
-            if li[j] + li[i] == (li[j] + li[i])[::-1]:
-                print(li[j] + li[i])
-                flag = True
-                break
-        if flag: break
-    if not flag: print(0)
+    x = int(input())
+    if x:
+        heappush(hq, -x) #이게되네
+    else:
+        print(0 if not hq else -heappop(hq))
+
+#11286번: 절댓값 힙
+input = __import__('sys').stdin.readline
+from heapq import heappush, heappop
+
+hq = []
+for _ in range(int(input())):
+    x = int(input())
+    if x:
+        heappush(hq, [abs(x), x])
+    else:
+        print(0 if not hq else heappop(hq)[1])
