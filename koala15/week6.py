bfab2fe9953cf288b957744ea22c71c5ad33e23c
haerxeong <haeryeong04@naver.com>
2024-08-21 15:32:01 +0900

    2606

--- koala15/week6.py
+++ koala15/week6.py
@@ -1,6 +1,35 @@
 #BFS, DFS
 
-#2644번: 촌수 계산
+#2606번: 바이러스
+from collections import deque
+
+def bfs(x):
+    Q = deque([x])
+    cnt = 0
+    visited[x] = True
+
+    while Q:
+        node = Q.popleft()
+        #visited[node] = True
+
+        for n in graph[node]:
+            if not visited[n]:
+                visited[n] = True
+                Q.append(n)
+                cnt += 1
+    return cnt
+
+n = int(input())
+graph = [[] for _ in range(n + 1)]
+visited = [False for _ in range(n + 1)]
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+print(bfs(1))
+
+#2644번: 촌수 계산 #pass
 from collections import deque
 
 n = int(input())
