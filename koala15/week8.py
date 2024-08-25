148e257c4b32efa2c8d40ac934ec92f96acd1b32
haerxeong <haeryeong04@naver.com>
2024-08-25 23:33:02 +0900

    1439

--- koala15/week8.py
+++ koala15/week8.py
@@ -13,6 +13,15 @@ N, M = map(int, input().split())
 li = sorted(set(map(int, input().split())))
 go([])
 
+#1439번: 뒤집기
+S = input()
+li = [0, 0]
+for i in range(len(S) - 1):
+    if S[i + 1] != S[i]:
+        li[int(S[i])] += 1
+li[int(S[-1])] += 1
+
+print(min(li))
 
 #20301번: 반전 요세푸스
 # N, K, M = map(int, input().split())
