dd5d6db7fc48732bf783dcbacc8e0cd86e736725
haerr <144093044+haerxeong@users.noreply.github.com>
2024-09-30 00:11:53 +0900

    [Silver V] Title: 덩치, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,12 @@
+N = int(input())
+li = [list(map(int, input().split())) for _ in range(N)]
+ans = [0] * N
+for i in range(N):  # 순위 당사자
+    rank = 1
+    for j in range(N):  # 비교 대상
+        if i != j:
+            if li[i][0] < li[j][0] and li[i][1] < li[j][1]:
+                rank += 1
+    ans[i] = rank
+
+print(' '.join(map(str, ans)))
